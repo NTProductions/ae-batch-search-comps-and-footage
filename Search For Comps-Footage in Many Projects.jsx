8d27a74d872ecf2243a07fe45f1c88ca3fedbc7f
Nate Lovell <Natelovell99@gmail.com>
2020-07-06 04:07:42 -0600

    Create Search For Comps-Footage in Many Projects.jsx

---
+++ Search For Comps-Footage in Many Projects.jsx
@@ -0,0 +1,40 @@
+﻿// Search for Comps/Footage in many projects
+
+var projects = getProjectFiles(Folder("~/Videos"));
+var compsAndFootage = getCompsAndFootage(projects);
+// [0] == comps
+// [1] == footage
+alert(compsAndFootage[0]);
+alert(compsAndFootage[1]);
+
+function getProjectFiles(folder) {
+    var files = folder.getFiles();
+    var foundProjects = [];
+    for(var i = 0; i < files.length; i++) {
+        if(files[i].name.slice(files[i].name.length-4, files[i].name.length) == ".aep") {
+            foundProjects.push(files[i]);
+            }
+        }
+    return foundProjects;
+    }
+
+function getCompsAndFootage(projectFiles) {
+    var foundComps = [];
+    var foundFootage = [];
+    app.beginSuppressDialogs();
+
+    for(var i = 0; i < projectFiles.length; i++) {
+    app.open(projectFiles[i]);
+    for(var e = 1; e <= app.project.numItems; e++) {
+        if(app.project.item(e) instanceof CompItem) {
+            foundComps.push(app.project.item(e).name);
+            }
+        if(app.project.item(e).file) {
+            foundFootage.push(app.project.item(e).name);
+            }
+        }
+    app.project.close(CloseOptions.DO_NOT_SAVE_CHANGES);
+    }
+    app.endSuppressDialogs(false);
+    return [foundComps, foundFootage];
+    }
